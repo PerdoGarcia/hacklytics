33c3349ab8606380b42f07f6966f1b4085d899be
PerdoGarcia <pedro_garcia@college.harvard.edu>
2025-02-22 00:19:14 -0500

    done with header

--- hacklytics/src/app/layout.js
+++ hacklytics/src/app/layout.js
@@ -1,6 +1,6 @@
 "use client"
 import { Geist, Geist_Mono } from "next/font/google";
-import Header from "../app/components/header";
+import Header from "../components/ui/header";
 import "./globals.css";
 
 const geistSans = Geist({
