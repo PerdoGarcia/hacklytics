42295bbad6171fd3bcf76be9833f072a03380b3e
PerdoGarcia <pedro_garcia@college.harvard.edu>
2025-02-22 00:24:50 -0500

    ui shadcn added

--- hacklytics/src/app/page.jsx
+++ hacklytics/src/app/page.jsx
@@ -1,31 +1,13 @@
 import Image from "next/image";
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 import { cn } from "@/lib/utils";
+import { CardWithForm } from "@/components/cardWithForm";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Card>
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+        <CardWithForm />
       </main>
     </div>
   );
