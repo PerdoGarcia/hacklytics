a6ac4989f94900e44b55aee3564cd1ad750b32b3
John Vizhco <john_vizhcoleon@college.harvard.edu>
2025-02-23 00:17:11 -0500

    css changes

--- hacklytics/frontend/src/app/markets/[series]/[ticker]/page.jsx
+++ hacklytics/frontend/src/app/markets/[series]/[ticker]/page.jsx
@@ -5,6 +5,7 @@ import MarketDashboard from '@/components/MarketDashboard';
 import Analyst from '@/components/Analyst';
 import MarketDetails from '@/components/MarketDetail';
 import BetPanel from '@/components/BetPanel';
+import mstyles from './markets.module.css';
 
 
 export default async function MarketPage({ params }) {
@@ -19,22 +20,22 @@ export default async function MarketPage({ params }) {
 
         if (!candleData) {
             console.log("No data received");
-            return <div>No data available</div>;
+            return <div className={mstyles.error}>No data available</div>;
         }
         return (
-            <div className="flex flex-col items-center w-full">
-                <h1 className="text-2xl font-semibold mb-4"></h1>
-                <div className="w-4/5 mx-auto space-y-6"> {/* Changed from w-2/3 to w-4/5 */}
-                    <div className="flex gap-6">
-                        <div className="flex-1">
+            <div className={mstyles.container}>
+                <h1 className={mstyles.header}>Market Details</h1>
+                <div className={mstyles.content}> {/* Changed from w-2/3 to w-4/5 */}
+                    <div className={mstyles.dashboard}>
+                        <div className={mstyles.panel}>
                             <MarketDashboard data={candleData.candlesticks || []} market={marketData || []} />
                         </div>
                     </div>
-                    <div className="flex gap-6">
-                        <div className="flex-1">
+                    <div className={mstyles.dashboard}>
+                        <div className={mstyles.panel}>
                             <Analyst />
                         </div>
-                        <div className="flex-1">
+                        <div className={mstyles.panel}>
                             {/* <BetPanel market={marketData || []} /> */}
                         </div>
                     </div>
@@ -44,6 +45,6 @@ export default async function MarketPage({ params }) {
 
     } catch (error) {
         console.error('Error fetching data:', error);
-        return <div>Error loading market data</div>;
+        return <div className={mstyles.error} >Error loading market data</div>;
     }
 }
